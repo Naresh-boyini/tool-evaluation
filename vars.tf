b0c6042bcff584188584dd9c02ac396b24546bda
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-05-14 01:13:41 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "naresh_cidr" {
+ 
+   type    = string
+}
+
+variable "naresh_tenancy" {
+ 
+  type    = string
+}
+
+variable "vpc_name" {
+  
+  type    = string
+}
+
+variable "subnet_cidr" {
+
+  type    = string
+}
+
+variable "name_subnet" {
+ 
+  type    = string
+}
