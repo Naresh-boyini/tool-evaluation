812a606f6c52de109f53417249142ff7dd9f4c78
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-05-17 01:56:03 +0530

    Update private2_ec2.tf

--- private2_ec2.tf
+++ private2_ec2.tf
@@ -2,14 +2,14 @@
 
 resource "aws_instance" "deploy_ec2_private2_instance" {
   ami           = "ami-0be48b687295f8bd6" # us-west-2
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
 
   network_interface {
     network_interface_id = aws_network_interface.deploy_private2_network_interface.id
     device_index         = 0
   }
 
-  key_name = "naresh"
+  key_name = "infra"
 
   tags = {
     Name = "kibana2"
@@ -53,12 +53,18 @@ resource "aws_security_group" "web_server_sg_private2_tf" {
 
   ingress {
     description = "All traffic ingress"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ ingress {
+    description = "All traffic ingress"
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
