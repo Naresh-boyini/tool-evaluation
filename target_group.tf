5b07f9f2cb680d79f7cf3638220ce629836dc08c
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-05-17 02:39:31 +0530

    Update target_group.tf

--- target_group.tf
+++ target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "deploy_tg" {
   name     = "tf-target-group"
-  port     = 80
+  port     = 5601
   protocol = "HTTP"
   vpc_id   = aws_vpc.deploy_vpc.id
 }
@@ -8,11 +8,11 @@ resource "aws_lb_target_group" "deploy_tg" {
 resource "aws_lb_target_group_attachment" "target_group_attachment_private1" {
   target_group_arn = aws_lb_target_group.deploy_tg.arn
   target_id        = aws_instance.deploy_ec2_private1_instance.id
-  port             = 80
+  port             = 5601
 }
 
 resource "aws_lb_target_group_attachment" "target_group_attachment_private2" {
   target_group_arn = aws_lb_target_group.deploy_tg.arn
   target_id        = aws_instance.deploy_ec2_private2_instance.id
-  port             = 80
+  port             = 5601
 }
